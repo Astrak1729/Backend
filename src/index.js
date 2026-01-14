005360c2f92c26af5b4c7ed76ce6aa328cb89832
Astrak1729 <rivikaishaira@gmail.com>
2026-01-14 22:22:35 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
 dotenv.config({
-  path:'./env'
-})
+  path: "./env",
+});
 
+// asyncrohnous method complete then it return a promise
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!", err);
+  });
 
 /*
 const app = express()
